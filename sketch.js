1193a1cc0757035c612b9c98a375bba8e178ee19
martinaesposito <martina.esposito@mail.polimi.it>
2022-10-17 00:38:28 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,28 @@
-//definisco una classe per la singola particella
-class Particle {
+
+//CLASSI
+
+//definisco una classe per le costellazioni
+class Star {
 	constructor(){
 		this.x = random(0,width);
 		this.y = random(0,height);
 		this.r 
 		this.brgt
 		this.alph = random(0,100);
-		this.xSpeed = random(-0.5,0.5);
-		this.ySpeed = random(-0.5,0.5);
+		this.xSpeed = random(-0.3,0.3);
+		this.ySpeed = random(-0.3,0.3);
 	}
-  
-//scrivo il metodo che crea la particella
-	createParticle() {
+
+	createStar() {
 		noStroke();
 		colorMode(HSB);
-
 		fill(100,0,this.brgt,this.alph);
 		circle(this.x,this.y,this.r);
 	}
   
-// scrivo la funziona che muove la particella
-	moveParticle() {
+	moveStar() {
 		if (this.x < 0 || this.x > width)
-//test per controllare che la particella rimanga entro i bordi della canvas
+//test per controllare che la stella rimanga entro i bordi della canvas
 //se tocca il bordo moltiplica la velocità per -1 e inverte la direzione
 			this.xSpeed*=-1;
 		if (this.y < 0 || this.y > height)
@@ -33,15 +33,15 @@ class Particle {
 	}
   
 // funzione che crea connessioni tra particelle che abbiamo una certa distanza tra di loro (40)
-	joinParticles(particles) {
-//per ciascun elemento dell'array particles chiamo una funzione che si attua nel momento in cui la chiamo
-		particles.forEach(element =>{
+	joinStars(stars) {
+//per ciascun elemento dell'array stars chiamo una funzione che si attua nel momento in cui la chiamo
+		stars.forEach(element =>{
 //definisco una variabile che calcola la distanza tra due punti dell'array
 		let dis = dist(this.x,this.y,element.x,element.y);
 
 //la distanza viene sempre calcolata e quando è inferiore a 40 allora viene disegnata una linea
 		if (dis < 40) {
-				strokeWeight(0.1);
+				strokeWeight(0.15);
 				stroke(100,0,100,100);
 				line(this.x,this.y,element.x,element.y)
 				}
@@ -50,51 +50,269 @@ class Particle {
 
 //funzione che illumina le particelle su cui il mouse fa hover
 //la logica con cui viene chiamata è la stessa della funzione che crea la linee
-	mouseParticles(particles) {
-		particles.forEach(element =>{
+	mouseStars(stars) {
+		stars.forEach(element =>{
 		let disM = dist(this.x,this.y,mouseX,mouseY);
 
 			if (disM < 40) {
-				this.r = random(0,3)+4;
-				this.brgt = random(0,1)+100;
+				this.r = random(0,3)+5;
+				this.brgt = random(0,1)+100
 			} 
-			else if (disM < 80) {
+			else if (disM < 120) {
 				this.r = random(0,2)+2;
 			} 
 			else {
-				this.r = random(1,2)
+				this.r = random(1,3)
 				}
 		});
 	}
 }
 
-// creo l'array delle particelle
-let particles = [];
+
+//definisco una classe per le stelline
+class SmallStar {
+	constructor(x, y) {
+	  this.x = random(0,width);
+	  this.y = random(0,height);
+	  this.r = random(0.1, 1.5);
+	  this.xSpeed = random(-0.5, 0.5);
+	  this.ySpeed = random(-0.5, 0.5);
+	  //this.c = color(random(128, 255), random(128, 255), random(128, 255));
+	}
+  
+	createSmallStar() {
+		noStroke();
+		fill("white");
+		circle(this.x,this.y,this.r);
+	}
+
+	moveSmallStar() {
+		if (this.x < 0 || this.x > width)
+			this.xSpeed*=-1;
+		if (this.y < 0 || this.y > height)
+			this.ySpeed*=-1;
+
+			this.x+=this.xSpeed;
+			this.y+=this.ySpeed;
+	}
+}	
+
+
+//ARRAY
+
+let stars = [];
+let SmallStars = [];
+let fallingstars = []
+
+let fallingstar
+
+
+//FUNZIONI
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 
-//numero degli elementi dell'array delle particelle cambia in base alla dimensione della canvas
-	for(let i = 0; i < width/3 ;i++){
-		particles.push(new Particle());
+//stelle
+	for(let i = 0; i < width/4 ;i++){
+		stars.push(new Star());
 	}
+
+//pianeti
+	for(let i = 0; i < width ;i++){
+	SmallStars.push(new SmallStar());
+	}
+
+//falling stars
+fallingstar = new FallingStar(100, 100);
+
+for(let i = 0; i < width/50; i++){
+	const randX = round(random(0, window.innerWidth));
+	const randY = round(random(0, window.innerHeight));
+	const randR = random(0.2, 2);
+	fallingstars.push(new StarField(randX, randY, randR));
 }
 
-function draw() {
+
+}
+
+function draw() {	
 	colorMode(RGB)
 	background(2,2,30);
 
-// elementi dell'array = numero di particelle definito in base alla dimensione della canvas
-// per tante volte quante sono le particelle chiamo le funzioni definite nella classe
-	for(let i = 0;i<particles.length;i++) {
-		particles[i].createParticle();
-		particles[i].moveParticle();
-		particles[i].joinParticles(particles.slice(i));
-
-		particles[i].mouseParticles(particles.slice(i));
+//stelle
+	for(let i = 0;i<stars.length;i++) {
+		stars[i].createStar();
+		stars[i].moveStar();
+		stars[i].joinStars(stars.slice(i));
+		stars[i].mouseStars(stars.slice(i));
 		}
-  }
+//pianeti
+	for(let i = 0;i<SmallStars.length;i++) {
+		SmallStars[i].createSmallStar();
+		SmallStars[i].moveSmallStar();
+		}
+
+//falling stars
+//applico a tutti gli elementi dell'array la funzione draw e creo un altro array che li contiene
+	fallingstars.map((element) => element.draw());
+
+//chiamo la funziona che crea una nuova stella solo se quella già creata è uscita dalla window
+	if(fallingstar.x > window.innerWidth + 100 ||
+		fallingstar.x < -100 || 
+		fallingstar.y > window.innerHeight + 100
+	   ){
+		fallingstar = new FallingStar(100, 100);
+   } else {
+	    fallingstar.draw();
+   }
+}
+
+function FallingStar(x, y) {
+ 
+   this.x = round(random(0, window.innerWidth));
+   this.xSpeed = round(random([-3,-8,3,8]));
+
+//le stelle vengono create sempre sopra alla window e procedono verso il basso 
+   this.y = -10;
+   this.ySpeed = random(1, 2);
+   
+   this.r = round(random(3, 5));
+
+   this.tail = [];
+   this.tailLength = 50;
+
+   this.Color = "#fce1b4";
+
+   
+   this.draw = function(){
+	   fill(this.Color);
+	   noStroke();
+	   circle(this.x, this.y, this.r);
+
+	   this.move();
+	   this.history();
+	   this.drawTail();
+   }
+   
+//scrivo la funzione che salva i valori x,y e r della falling star nell'array tail per generare la coda
+   this.history = function() {
+	    this.tail.push({x: this.x, y: this.y, r: this.r});
+
+//se la lunghezza dell'array è più lunga di 60 elementi allora cancella il primo elemento dell'array
+//questo è ciò che permette alla coda di scomparire man mano che la stella si muove
+		if(this.tail.length > this.tailLength) {
+			this.tail.shift(); }
+   }
+
+//disegno la coda
+//per la lunghezza della coda disegno un cerchio assumendo i valori di x, y e r salvati nell'array 
+   this.drawTail = function(){
+	   for(i = this.tail.length - 1; i > 0; i--){
+		   fill("white");
+		   noStroke();
+		   circle(this.tail[i].x, this.tail[i].y, this.tail[i].r);
+		   
+		   //calculate the proper numer to reduce radius to 0
+		   const radiusReducer = this.tail[i].r / this.tailLength*2;
+		   this.tail[i].r -= radiusReducer;
+	   }
+   }
+   
+   this.move = function(){
+	   this.x += this.xSpeed;
+	   this.y += this.ySpeed;
+   }
+}
+
+function StarField(x, y, r) {
+   this.x = x;
+   this.y = y;
+   this.r = r;
+   
+   this.draw = function(){
+	   circle(this.x, this.y, this.r)
+	   fill(255);	
+   }
+
+}
+
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
   }
+
+
+
+
+
+
+
+
+
+
+
+
+/*SOLI
+
+  //definisco una classe per i soli
+class Sun {
+	constructor() {
+	  this.x = random(0,width);
+	  this.y = random(0,height);
+	  this.r = random(30, 50);
+	  this.xSpeed = 20
+	  this.ySpeed = 20;
+	}
+
+	createSun(){
+		noStroke();
+		fill(255, 255, 0);
+		circle(width / 2, height / 2, 50);
+	}
+
+	moveSun() {
+		if (this.x < 0 || this.x > width)
+			this.xSpeed*=-1;
+		if (this.y < 0 || this.y > height)
+			this.ySpeed*=-1;
+
+			this.x+=this.xSpeed;
+			this.y+=this.ySpeed;
+	}
+}
+
+//soli	
+	for(let i = 0; i < 5 ;i++){
+	suns.push(new Sun());
+  	}
+	
+//soli
+	for(let i = 0;i<suns.length;i++) {
+		suns[i].createSun();
+		suns[i].moveSun();
+		}
+
+//let suns = [];
+
+*/
+
+
+/*PIANETI
+
+	orbitPlanet(){
+		planets.forEach(element =>{
+		let distPlanet = dist(this.x, this.y, mouseX, mouseY);
+
+		if (distPlanet < 80) {
+			this.xSpeed += (mouseX - this.x) / distPlanet;
+			this.ySpeed += (mouseY - this.y) / distPlanet;
+		
+			this.x += this.xSpeed;
+			this.y += this.ySpeed;
+		}
+	  })
+	}
+	
+		
+	planets[i].orbitPlanet(planets.slice(i));
+	*/
